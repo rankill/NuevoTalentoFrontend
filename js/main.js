18b9a6588eeb044365223d9fd4d93783068c9d46
legacyrankill <Hardcore19>
2016-09-16 03:44:21 -0500

    Finalizado el diseño de menu de navegacion de submenus de la cabecera de la pagina web

--- js/main.js
+++ js/main.js
@@ -25,4 +25,24 @@
         },false);
     });
 
+    function loadJSON(_jsonFile, _callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', _jsonFile, true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                _callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+
+    loadJSON('resources/data/menu.json', function(response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
+    });
+
 }());
